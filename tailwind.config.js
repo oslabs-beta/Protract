9c1c1aa559a1474150dac7332c5e020c7b38075b
Vander <vander@Vanders-MacBook-Pro.local>
2023-06-13 12:54:11 -0700

    practicing tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/client/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/client/*.{js,ts,jsx,tsx}', './src/client/components/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
