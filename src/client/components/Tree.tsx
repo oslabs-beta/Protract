dfa657bd345a55ba72402a9a389a1ab7c7888405
Douglas Yao <doug.yao@gmail.com>
2023-06-14 09:42:00 -0700

    create dummy data

--- src/client/components/Tree.tsx
+++ src/client/components/Tree.tsx
@@ -1,7 +1,54 @@
+
+
+
 export default function Tree() {
+
+  const dummy = [
+    {
+      currentProject: {
+
+        children: [
+          {
+            app: {
+              children: [
+                {
+                  header: {
+                    children: [],
+                    code: 'header-code;'
+                   },
+                },
+                {
+                  main: {
+                    children: [
+                      {
+                        component1: {
+                          children: [],
+                          code: 'component1-code'
+                        }
+                      }
+                    ],
+                    code: '<component1/>\nmain-code;'
+                  }
+                },
+                {
+                  footer: {
+                    children: [],
+                    code: 'footer-code;'
+                  }
+                }
+              ],
+            code: '<header/>\n<main/>\n<footer/>'
+            }
+          }
+        ],
+        code: '<app/>'
+      }
+    }
+  ];
+
   return (
-    <div className="">
-      Tree
+    <div className="w-full h-full p-3 bg-red-100">
+
     </div>
   );
 }
