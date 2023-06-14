df58ba98e41034156e2e139630dd0960dad34da2
Svaughn4418 <svaughn4418@gmail.com>
2023-06-13 20:07:04 -0500

    Co-authored-by: Don <Donlebon@users.noreply.github.com>

--- src/client/components/Navbar.tsx
+++ src/client/components/Navbar.tsx
@@ -1,5 +1,5 @@
-export default function Navbar() {
-  // return <nav className="border-solid border-2 border-sky-500">Navbar</nav>;
-  // <nav className="flex flex-row text-3xl font-bold underline">Navbar</nav>
-  return <nav className="border-solid border-2 border-red-500">Navbar</nav>;
-}
+// export default function Navbar() {
+//   return (
+//   <nav className="border-solid border-2 border-red-500">Navbar</nav>
+//   );
+// }
--- src/server/main.ts
+++ src/server/main.ts
@@ -1,12 +1,43 @@
+//--NPM Packages
 import express from "express";
 import ViteExpress from "vite-express";
+// const path = require('')
 
+
+//--Import Routes & Controllers
 const app = express();
+
+//--Connect to Port
+ViteExpress.listen(app, 3000, () =>
+  console.log("Server is listening on port 3000...")
+);
+//--connect to DB
+// const MONGO_URI =
+//mongoose
+//.connect(MONGO_URI, { dbName: 'practice' })
+//.then(()=>console.log('connection to database successful'))
+//.catch((err) =>
+//console.log(`database connection not successful. Received this ${err}`)
+//);
+
+//Parse incoming request
+// app.use('/client', express.static(path.join(__dirname, '../client')))
+// app.use(express.json());
+// app.use(express.urlencoded());
+
+
+//Route Handlers
+//-- general Routes
 
 app.get("/hello", (_, res) => {
   res.send("Hello Vite + React + TypeScript!");
 });
 
-ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000...")
-);
+//Authentication routes
+// app.get('/login', (req, res) => {
+//   return res.sendFile(path.join(__dirname, '../client/login.html'));
+// });
+
+// app.get('/signup', (req, res) => {
+//   return res.sendFile(path.join(__dirname, '../client/signup.html'));
+// });
