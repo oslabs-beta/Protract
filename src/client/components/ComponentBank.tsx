b0713ebeade999f58a63c48630a746c3223c0360
peter <tranpeter1095@gmail.com>
2023-06-13 19:24:20 -0600

    refactored bank to ul, li

--- src/client/components/ComponentBank.tsx
+++ src/client/components/ComponentBank.tsx
@@ -24,8 +24,8 @@ export default function ComponentBank() {
   });
 
   return (
-    <div className="scrollbar-thin scrollbar-thumb-slate-300 scrollbar-track-slate-200 border-solid border-2 border-sky-400 my-5 h-3/4 overflow-y-scroll">
+    <ul className="scrollbar-thin scrollbar-thumb-slate-300 scrollbar-track-slate-200 border-solid border-2 border-sky-400 my-5 h-3/4 overflow-y-scroll">
       {elList}
-    </div>
+    </ul>
   );
 }
