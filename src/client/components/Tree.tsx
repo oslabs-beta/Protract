7393c5547f66702497a3d2e435e2a245dd97e8df
Douglas Yao <doug.yao@gmail.com>
2023-06-14 13:29:45 -0700

    start logic for rendering tree components

--- src/client/components/Tree.tsx
+++ src/client/components/Tree.tsx
@@ -1,71 +1,62 @@
+import React from 'react';
+import { Component } from './dummyData';
 
+interface TreeProps {
+  root: Component;
+}
 
+const Tree: React.FC<TreeProps> = ({ root }) => {
+  console.log('root', root);
+  const renderComponent = (component: Component) => {
 
-  import { currentProject, Component } from './dummyData';
-
-  const renderComponent = (component: Component): JSX.Element => {
-    const { code, children } = component;
-    return (
-      <div key={code}>
-        {code}
-        {children.map((child) => renderComponent(child))}
-      </div>
-    );
   };
 
-  const Tree: React.FC<any> = () => {
-    return (
-      <div className="w-full h-full p-3 bg-red-100">
-        {/* {currentProject.map((project, index) => (
-          <div key={index}>{renderComponent(project)}</div>
-        ))} */}
-        { currentProject }
-      </div>
-    );
-  };
+  return <div>{renderComponent(root)}</div>;
+  // return <pre>{root}</pre>
+};
 
-  export default Tree;
+export default Tree;
 
 
-    // const dummy = [
-  //   {
-  //     currentProject: {
+  // const dummy = [
+//   {
+//     currentProject: {
 
-  //       children: [
-  //         {
-  //           app: {
-  //             children: [
-  //               {
-  //                 header: {
-  //                   children: [],
-  //                   code: 'header-code;'
-  //                  },
-  //               },
-  //               {
-  //                 main: {
-  //                   children: [
-  //                     {
-  //                       component1: {
-  //                         children: [],
-  //                         code: 'component1-code'
-  //                       }
-  //                     }
-  //                   ],
-  //                   code: '<component1/>\nmain-code;'
-  //                 }
-  //               },
-  //               {
-  //                 footer: {
-  //                   children: [],
-  //                   code: 'footer-code;'
-  //                 }
-  //               }
-  //             ],
-  //           code: '<header/>\n<main/>\n<footer/>'
-  //           }
-  //         }
-  //       ],
-  //       code: '<app/>'
-  //     }
-  //   }
-  // ];
+//       children: [
+//         {
+//           app: {
+//             children: [
+//               {
+//                 header: {
+//                   children: [],
+//                   code: 'header-code;'
+//                  },
+//               },
+//               {
+//                 main: {
+//                   children: [
+//                     {
+//                       component1: {
+//                         children: [],
+//                         code: 'component1-code'
+//                       }
+//                     }
+//                   ],
+//                   code: '<component1/>\nmain-code;'
+//                 }
+//               },
+//               {
+//                 footer: {
+//                   children: [],
+//                   code: 'footer-code;'
+//                 }
+//               }
+//             ],
+//           code: '<header/>\n<main/>\n<footer/>'
+//           }
+//         }
+//       ],
+//       code: '<app/>'
+//     }
+//   }
+// ];
