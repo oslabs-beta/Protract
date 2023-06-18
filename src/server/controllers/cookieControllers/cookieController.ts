0fe1bdd11e93fe95b305c0fb567734de3aa329df
Don <delidinos@gmail.com>
2023-06-18 11:18:02 -0700

    complete loggedIn and logout

--- src/server/controllers/cookieControllers/cookieController.ts
+++ src/server/controllers/cookieControllers/cookieController.ts
@@ -1,22 +1,12 @@
 import express, { Request, Response, NextFunction, RequestHandler } from 'express';
 
 interface CookieController {
-    setCookie: (req: Request, res: Response, next: NextFunction) => Promise<void>;
     setSSIDCookie: (req: Request, res: Response, next: NextFunction) => Promise<void>;
 }
 const cookieController: CookieController = {
 
-// setCookie Test
-
-setCookie: async (req, res, next) => {
-    console.log('cookie created')
-    res.cookie('Project', 'Protract');
-    return next();
-  },
-
 // Cookie is set after user successfully signs up or logs in
 // setSSIDCookie - store the user id in a cookie
-
 
 setSSIDCookie: async (req, res, next) => {
     const options = {
