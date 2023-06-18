42661fef037a4cfaf39402925232a74b1eb1f591
Douglas Yao <doug.yao@gmail.com>
2023-06-18 09:15:08 -0700

    update dummy data

--- src/client/components/dummyData.ts
+++ src/client/components/dummyData.ts
@@ -1,51 +1,106 @@
-
+const app = {value: 'app', codeStart: '<app>', codeEnd: '</app>', children: []}
 
 export interface Component {
-  [key: string]: {
-    children: Component[];
-    code: string;
-  };
+  value: string;
+  id: string;
+  code: string;
+  children: Component[];
+  onClick?: () => void;
 }
 
+// export interface Component {
+//   [key: string]: {
+//     children: Component[];
+//     code: string;
+//   };
+// }
+
 export const currentProject: Component = {
-    app: {
+  value: 'app',
+  id: '0',
+  code: '<app/>',
+  children: [
+    {
+      value: 'comp1',
+      id: '1',
+      code: '<comp1/>',
       children: [
         {
-          main: {
-            children: [
-              {
-                header: {
-                  children: [],
-                  code: 'header-code;'
-                 },
-              },
-              {
-                mainDiv: {
-                  children: [
-                    {
-                      component1: {
-                        children: [],
-                        code: 'component1-code'
-                      }
-                    }
-                  ],
-                  code: '<component1/>\nmainDiv-code;'
-                }
-              },
-              {
-                footer: {
-                  children: [],
-                  code: 'footer-code;'
-                }
-              }
-            ],
-          code: '<header/>\n<mainDiv/>\n<footer/>'
-          }
+          value: 'comp3',
+          id: '3',
+          code: '<comp3/>',
+          children: []
         }
-      ],
-      code: '<app>\n<main/>\n</app>'
+      ]
+    },
+    {
+      value: 'comp2',
+      id: '2',
+      code: '<comp2/>',
+      children: [
+        {
+          value: 'comp4',
+          id: '4',
+          code: '<comp4/>',
+          children: [
+            {
+              value: 'comp5',
+              id: '5',
+              code: '<comp5/>',
+              children: []
+            }
+          ]
+        }
+      ]
+    },
+    {
+      value: 'bloop',
+      id: 'bloop',
+      code: '<bloop/>',
+      children: []
     }
-};
+  ]
+}
+
+// export const currentProject: Component = {
+//     app: {
+//       children: [
+//         {
+//           main: {
+//             children: [
+//               {
+//                 header: {
+//                   children: [],
+//                   code: 'header-code;'
+//                  },
+//               },
+//               {
+//                 mainDiv: {
+//                   children: [
+//                     {
+//                       component1: {
+//                         children: [],
+//                         code: 'component1-code'
+//                       }
+//                     }
+//                   ],
+//                   code: '<component1/>\nmainDiv-code;'
+//                 }
+//               },
+//               {
+//                 footer: {
+//                   children: [],
+//                   code: 'footer-code;'
+//                 }
+//               }
+//             ],
+//           code: '<header/>\n<mainDiv/>\n<footer/>'
+//           }
+//         }
+//       ],
+//       code: '<app>\n<main/>\n</app>'
+//     }
+// };
 
 
 // export const currentProject: Component =
