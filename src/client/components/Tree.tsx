25b14b2da9bb4a8bfe2153a0c169cf4b07ed7e05
Douglas Yao <doug.yao@gmail.com>
2023-06-19 09:17:21 -0700

    check for canEnter property on currentComponent before populating tree node

--- src/client/components/Tree.tsx
+++ src/client/components/Tree.tsx
@@ -12,14 +12,17 @@ interface TreeProps {
 
 const Tree: React.FC<TreeProps> = ({ comps, depth = 0 }) => {
 
-  const { setItems, setCurrComp, setChildren } = useContext(PlaygroundContext);
+  const { currComp, setCurrComp, setChildren } = useContext(PlaygroundContext);
 
   console.log('in tree');
   console.log('comps', comps);
+  console.log('currComp', currComp);
 
   function handleClick(comp) {
     setCurrComp(comp)
-    if (comp.children.length > 0) setChildren(comp.children);
+    // if (comp.children.length > 0) setChildren(comp.children);
+    setChildren(comp.children)
+    console.log('currComp in tree handleClick')
   }
 
   /**
@@ -30,16 +33,20 @@ const Tree: React.FC<TreeProps> = ({ comps, depth = 0 }) => {
    */
   const renderTree = (currentComponent: Comp | Item, currentDepth: number) => {
     const { value, children } = currentComponent;
+    console.log('currentComponent', currentComponent);
 
     return (
       <div className={`ml-${currentDepth * 4} flex flex-col items-center`}>
-        <div onClick={() => handleClick(currentComponent)}>
-          <strong>{value}</strong>
-        </div>
+        {currentComponent.canEnter && (
+          <div onClick={() => handleClick(currentComponent)} className="mt-4">
+            <strong>{value}</strong>
+          </div>
+        )}
+
         {children && children.length > 0 && (
           <div className="flex flex-col items-center">
             {children.map((child, index) => (
-              <div key={index} className="mt-2">
+              <div key={index} className="">
                 <Tree comps={child} depth={currentDepth + 1} />
               </div>
             ))}
