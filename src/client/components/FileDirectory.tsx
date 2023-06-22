3aae9d45a454402a4877786f914beeabf5641739
peter <tranpeter1095@gmail.com>
2023-06-22 17:36:13 -0600

    fixed filedir scroll

--- src/client/components/FileDirectory.tsx
+++ src/client/components/FileDirectory.tsx
@@ -1,43 +1,50 @@
-import { Item } from "../../types";
-import { PlaygroundContext } from "./Playground";
-import { useContext } from "react";
+import { Item } from '../../types';
+import { PlaygroundContext } from './Playground';
+import { useContext } from 'react';
 
-export default function FileDirectory(props: { comps: Item, depth: number }) {
+export default function FileDirectory(props: { comps: Item; depth: number }) {
   const { setCurrComp, setChildren } = useContext(PlaygroundContext);
-  const {comps, depth} = props;
+  const { comps, depth } = props;
 
   function handleClick(comp: Item) {
-    setCurrComp(comp)
-    setChildren(comp.children)
+    setCurrComp(comp);
+    setChildren(comp.children);
   }
 
- const renderTree = (currentComponent: Item) => {
+  const renderTree = (currentComponent: Item) => {
     const { value, children } = currentComponent;
 
     return (
-        <div>
+      <div>
         <div className={` border-l ${depth > 0 ? 'ml-4 pl-1' : ''} mx-4`}>
-          <strong style={{ cursor: 'pointer' }} onClick={() => handleClick(currentComponent)}>{value}</strong>
+          <strong
+            style={{ cursor: 'pointer' }}
+            onClick={() => handleClick(currentComponent)}
+          >
+            {value}
+          </strong>
           {children && children.length > 0 && (
             <div>
-              {children.map((child, index) => (
-                child.canEnter &&
-                <div key={index} className={''}>
-                  <div>
-                    <FileDirectory comps={child} depth={depth + 1} />
-                  </div>
-                </div>
-              ))}
+              {children.map(
+                (child, index) =>
+                  child.canEnter && (
+                    <div key={index} className={''}>
+                      <div>
+                        <FileDirectory comps={child} depth={depth + 1} />
+                      </div>
+                    </div>
+                  )
+              )}
             </div>
           )}
         </div>
-        </div>
+      </div>
     );
   };
 
   return (
-    <div className="overflow-y-scroll scrollbar-thin scrollbar-thumb-white scrollbar-track-white">
+    <div className="max-h-48 overflow-y-scroll scrollbar-thin scrollbar-track-white scrollbar-thumb-white">
       {renderTree(comps)}
-      </div>
+    </div>
   );
-};
+}
