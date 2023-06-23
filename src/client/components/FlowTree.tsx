b11a2dd7078aa22954916580cd4645619abbbe63
Vander <vander@Vanders-MacBook-Pro.local>
2023-06-23 12:10:25 -0700

    modified tree component formatting, and active project load

--- src/client/components/FlowTree.tsx
+++ src/client/components/FlowTree.tsx
@@ -36,35 +36,53 @@ const FlowTree: React.FC<TreeProps> = ({ root }) => {
 
   const elements = convertDataToElements(root);
 
+
+  const renderRectSvgNode = ({ nodeDatum, toggleNode }) => (
+    <g>
+      <rect width="16" height="16" x="-8" rx="20" ry="20" fill="#b91c1c" onClick={() => handleClick(nodeDatum) } />
+      <text fill="black" strokeWidth="1" x="13" y="13" onClick={() => handleClick(nodeDatum)}>
+        {nodeDatum.name}
+      </text>
+    </g>
+  );
+
+
   // Styling currently does not apply. Consider styling in a css/sass file, then importing?
   return (
     <div ref={treeContainerRef} style={{ width: '100%', height: '100%' }}>
       <Tree
         data={elements}
         translate={{ x: 175, y: 40 }}
-        nodeSize={{ x: 200, y: 100 }}
-        separation={{ siblings: 1.0, nonSiblings: 1.0 }}
+        nodeSize={{ x: 100, y: 50 }}
+        separation={{ siblings: .9, nonSiblings: 1.0 }}
         collapsible={false}
         zoomable={true}
         orientation="vertical"
-        pathFunc="step"
-        depthFactor={200}
+        pathFunc="diagonal"
         onNodeClick={(nodeData, e) => handleClick(nodeData.data)}
+        rootNodeClassName='nodeRoot'
+        branchNodeClassName='nodeBranch'
+        leafNodeClassName='nodeLeaf'
+        depthFactor={125}
+        zoomable={false}
+        zoom={.9}
+        renderCustomNodeElement={renderRectSvgNode}
+      // scaleExtent={{max:.9, min:.9}}
 
-        // nodeSvgShape={{
-        //   shape: 'circle',
-        //   shapeProps: {
-        //     r: 10,
-        //     fill: '#B91C1C',
-        //     stroke: '#000',
-        //   },
-        //   label: {
-        //     textAnchor: 'end',
-        //     fontSize: 14,
-        //     fontWeight: 700,
-        //     transform: 'translate(-15px, 0)',
-        //   },
-        // }}
+      // nodeSvgShape={{
+      //   shape: 'circle',
+      //   shapeProps: {
+      //     r: 10,
+      //     fill: '#B91C1C',
+      //     stroke: '#000',
+      //   },
+      //   label: {
+      //     textAnchor: 'end',
+      //     fontSize: 14,
+      //     fontWeight: 700,
+      //     transform: 'translate(-15px, 0)',
+      //   },
+      // }}
       />
     </div>
   );
@@ -82,13 +100,13 @@ const convertDataToElements = (root: Item) => {
     }
 
     // create a new element, in the fashion of d3-tree syntax
-    const element:RawNodeDatum = { ...node, name: value.toString(), children: [] };
+    const element: RawNodeDatum = { ...node, name: value.toString(), children: [] };
 
     // loop through children arrays if present
     if (children && children.length > 0) {
       children.forEach((child) => {
         // childElement will equal recursive calls to traverse each child, resulting in subsequent element arrays being populated
-        const childElement:RawNodeDatum = traverse(child);
+        const childElement: RawNodeDatum = traverse(child);
         // if childElement is not null, push to the element's children array
         if (childElement) {
           element.children.push(childElement);
