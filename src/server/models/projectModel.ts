e3b4425d8f3adef60153909ae3c7b9b89df18680
peter <tranpeter1095@gmail.com>
2023-06-23 18:20:05 -0600

    fixing some typing

--- src/server/models/projectModel.ts
+++ src/server/models/projectModel.ts
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+import { Item } from "../../types";
+
+interface projectSchema {
+  title: string;
+  root: Array<Item>;
+  users: Array<String>;
+}
+
 const projectSchema = new Schema({
   title: {type: String, required: true},
-  root: {type: Array, required: true},
+  root: {type: Array<Item>, required: true},
   users: {type: Array<String>, required: true}
 })
 
