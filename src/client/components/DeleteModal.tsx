edb5e0a19d0643318bdcea3fcc27e3c60e954472
peter <tranpeter1095@gmail.com>
2023-06-26 15:48:14 -0600

    wrote a large e2e test

--- src/client/components/DeleteModal.tsx
+++ src/client/components/DeleteModal.tsx
@@ -1,27 +1,48 @@
-import { UniqueIdentifier } from "@dnd-kit/core";
-import React, {useRef} from "react";
-import ReactDOM from "react-dom";
+import { UniqueIdentifier } from '@dnd-kit/core';
+import React, { useRef } from 'react';
+import ReactDOM from 'react-dom';
 
-export default function DeleteModal(props: {value: UniqueIdentifier, handleDelete:() => void, handleCancel:() => void}) {
+export default function DeleteModal(props: {
+  value: UniqueIdentifier;
+  handleDelete: () => void;
+  handleCancel: () => void;
+}) {
+  const { value, handleDelete, handleCancel } = props;
 
-  const {value, handleDelete, handleCancel} = props;
-
-  const modalRef= useRef<HTMLDivElement>(null);;
+  const modalRef = useRef<HTMLDivElement>(null);
   const closeModal = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     if (e.target === modalRef.current) {
       handleCancel();
     }
-  }
+  };
 
-  return ReactDOM.createPortal (
-    <div className="fixed inset-0 h-screen w-screen flex items-center justify-center bg-black bg-opacity-40" ref={modalRef} onClick={(e) => closeModal(e)}>
-        <div className="h-28 w-48 z-50 bg-white border-0 rounded text-center pt-4 ">Delete {value}?
+  return ReactDOM.createPortal(
+    <div
+      className="fixed inset-0 flex h-screen w-screen items-center justify-center bg-black bg-opacity-40"
+      ref={modalRef}
+      onClick={(e) => closeModal(e)}
+    >
+      <div
+        id="deleteModal"
+        className="z-50 h-28 w-48 rounded border-0 bg-white pt-4 text-center "
+      >
+        Delete {value}?
         <div className="m-7 space-x-8 ">
-          <button className="text-white bg-red-800 border-0 border-red-800 w-1/4 min-w-fit px-2 rounded" onClick={() => handleDelete()}>Yes</button>
-          <button className="text-red-800 bg-white border-2 border-white w-1/4 min-w-fit rounded" onClick={() => handleCancel()}>No</button>
+          <button
+            className="w-1/4 min-w-fit rounded border-0 border-red-800 bg-red-800 px-2 text-white"
+            onClick={() => handleDelete()}
+          >
+            Yes
+          </button>
+          <button
+            className="w-1/4 min-w-fit rounded border-2 border-white bg-white text-red-800"
+            onClick={() => handleCancel()}
+          >
+            No
+          </button>
         </div>
-        </div>
-    </div>, 
-        document.getElementById('portal') as Element
-  )
+      </div>
+    </div>,
+    document.getElementById('portal') as Element
+  );
 }
