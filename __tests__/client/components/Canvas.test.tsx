a9954ec2c6a6319e39433f9d8cd391ca4617f41d
peter <tranpeter1095@gmail.com>
2023-06-26 11:24:16 -0600

    added cypress

--- __tests__/client/components/Canvas.test.tsx
+++ __tests__/client/components/Canvas.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 import Canvas from '../../../src/client/components/Canvas';
 
@@ -27,8 +28,11 @@ describe('Canvas', () => {
 
   test('renders list based on children of currComp', async () => {
     await waitFor(() => {
-      const liElements = screen.getAllByRole('button');
-      expect(liElements).toHaveLength(4);
+      const ul = screen.getByRole('list');
+      expect(ul).toBeInTheDocument();
+      expect(ul.children.length).toBe(2);
+      expect(ul.children[0]).toHaveTextContent('element1');
+      expect(ul.children[1]).toHaveTextContent('element2');
     });
   });
 });
---
+++ cypress/component/Canvas.cy.tsx
@@ -0,0 +1,5 @@
+describe('Canvas.cy.tsx', () => {
+  it('playground', () => {
+    // cy.mount()
+  })
+})
